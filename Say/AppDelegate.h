32f6a2eca88a3f9087791d189abab9ef6c5b5ac9
Aleksander Skjølsvik <Aleksander@aleksander-skjlsviks-macbook-pro.local>
2014-11-01 08:38:33 -0400

    Initial Commit

---
+++ Say/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Say
+//
+//  Created by Aleksander Skjølsvik on 01.11.14.
+//  Copyright (c) 2014 alekApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Say/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Say
+//
+//  Created by Aleksander Skjølsvik on 01.11.14.
+//  Copyright (c) 2014 alekApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
